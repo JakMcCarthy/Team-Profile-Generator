09c609b6f8a994c4ba8ea017fca1986330060c16
JakMcCarthy <90810973+JakMcCarthy@users.noreply.github.com>
2022-09-07 21:17:43 -0400

    Change path

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const fs = require('fs'); 
 const inquirer = require('inquirer');
-const path = require("path");
 const generateHTML = require('./src/generateHTML');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
